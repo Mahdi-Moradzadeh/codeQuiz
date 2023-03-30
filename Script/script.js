2f49d08611d12476ae353e0dcdba2cc8b7b431dd
Mohammadmahdi Moradzadeh <126528880+Mahdi-Moradzadeh@users.noreply.github.com>
2023-03-30 19:28:43 -0400

    The second draft of the project: missing timer

--- Script/script.js
+++ Script/script.js
@@ -1,84 +1,143 @@
-let userChoice = 1;
+let userChoice = 0;
 let introMessage = {
-    question: "Do you want to start?"
+    question: "Do you want to start? Press the start button above"
 };
+let SCORE = 0;
+let timer = 0;
+let totalTime = 100;
 
-let questionOne = {
-    question: "what is 2+2?",
-    ansCorrect: "4",
-    ansIncorrectOne: "3",
-    ansIncorrectTwo: "5",
-    ansIncorrectThree: "2"
-};
+let counter = setInterval(function timeInterval(totalTime) {
+}, 1000);
 
-let questionTwo = {
-    question: "what is 2*2?",
-    ansCorrect: "4",
-    ansIncorrectOne: "3",
-    ansIncorrectTwo: "5",
-    ansIncorrectThree: "2"
-};
+const allQuestions = [
+{
+    question: "What is 2+2?",
+    answers: [
+        {ans: 4, result: true},
+        {ans: 3, result: false},
+        {ans: 5, result: false},
+        {ans: 2, result: false}]
 
-let questionThree = {
+},
+
+{
+    question: "What is 2*2?",
+    answers: [
+        {ans: 6, result: false},
+        {ans: 3, result: false},
+        {ans: 4, result: true},
+        {ans: 2, result: false}]
+},
+
+{
     question: "what is 2/2?",
-    ansCorrect: "1",
-    ansIncorrectTwo: "0",
-    ansIncorrectThree: "2",
-    ansIncorrectOne: "0.5"
-};
+    answers: [
+        {ans: 1, result: true},
+        {ans: 0, result: false},
+        {ans: 2, result: false},
+        {ans: 0.5, result: false}]
+},
 
-let questionFour = {
+{
     question: "what is 2%2?",
-    ansCorrect: "0",
-    ansIncorrectThree: "1",
-    ansIncorrectOne: "4",
-    ansIncorrectTwo: "2"
+    answers: [
+        {ans: 1, result: false},
+        {ans: 4, result: false},
+        {ans: 2, result: false},
+        {ans: 0, result: true}]
+}
+]
 
-};
+let A = document.getElementById("buttonA");
+let B = document.getElementById("buttonB");
+let C = document.getElementById("buttonC");
+let D = document.getElementById("buttonD");
+let start = document.getElementById("start");
+let submit = document.getElementById("submit");
+let score = document.getElementById("score");
+let next = document.getElementById("next");
+let previous = document.getElementById("previous");
 
-postQuestion(userChoice);
+document.getElementById("question").innerHTML = introMessage.question;
+A.style.visibility = 'hidden';
+B.style.visibility = 'hidden';
+C.style.visibility = 'hidden';
+D.style.visibility = 'hidden';
 
-function postQuestion (questionNumber){
-    document.getElementById("question").innerHTML = introMessage.question;
-    document.getElementById("ansA").innerHTML = "YES";
-    document.getElementById("ansB").innerHTML = "NO";
-    document.getElementById("buttonC").style.visibility = 'hidden';
-    document.getElementById("buttonD").style.visibility = 'hidden';
+start.onclick = quizStarter;
 
-    if(document.getElementById("buttonA").clicked == true){
-        alert("Quiz will start now");
+function buttonClick(e){
+    userChoice++;
+    let userAns = e.srcElement.value;
+    console.log(userAns);
+    if (userAns === 'true')
+    SCORE++;   
+    // else for timer
+    quizStarter(userChoice);
+}
+
+function nextQ(g){
+    if(userChoice<4)
+    userChoice++;
+    quizStarter(userChoice);
+}
+
+function previousQ(h){
+    if(userChoice>1)
+    userChoice--;
+    quizStarter(userChoice);
+}
+
+function submission (f){
+    alert("Your Quiz has been submitted and your score is displayed");    
+}
+
+function timeInterval(totalTime){
+    totalTime--;
+    timerCounter.innerText = counter;
+    quizStarter(userChoice);
+}
+
+function quizStarter(){
+
     
-        document.getElementById("buttonC").style.visibility = 'visible';
-        document.getElementById("buttonD").style.visibility = 'visible';
-        if(questionNumber == 1){
-        document.getElementById("question").innerHTML = questionOne.question;
-        document.getElementById("ansA").innerHTML = questionOne.ansCorrect;
-        document.getElementById("ansB").innerHTML = questionOne.ansIncorrectOne;
-        document.getElementById("ansC").innerHTML = questionOne.ansIncorrectTwo;
-        document.getElementById("ansD").innerHTML = questionOne.ansIncorrectThree;
-        }
-        else if (questionNumber == 2){
-        document.getElementById("question").innerHTML = questionTwo.question;
-        document.getElementById("ansB").innerHTML = questionTwo.ansCorrect;
-        document.getElementById("ansA").innerHTML = questionTwo.ansIncorrectOne;
-        document.getElementById("ansC").innerHTML = questionTwo.ansIncorrectTwo;
-        document.getElementById("ansD").innerHTML = questionTwo.ansIncorrectThree;
+    score.innerText = SCORE;
 
-        }
-        else if(questionNumber == 3){
-        document.getElementById("question").innerHTML = questionThree.question;
-        document.getElementById("ansD").innerHTML = questionThree.ansCorrect;
-        document.getElementById("ansC").innerHTML = questionThree.ansIncorrectOne;
-        document.getElementById("ansA").innerHTML = questionThree.ansIncorrectTwo;
-        document.getElementById("ansB").innerHTML = questionThree.ansIncorrectThree;
+    if (userChoice>4)
+    {
 
-        }
-        else if (questionNumber == 4){
-        document.getElementById("question").innerHTML = questionFour.question;
-        document.getElementById("ansB").innerHTML = questionFour.ansCorrect;
-        document.getElementById("ansA").innerHTML = questionFour.ansIncorrectOne;
-        document.getElementById("ansD").innerHTML = questionFour.ansIncorrectTwo;
-        document.getElementById("ansC").innerHTML = questionFour.ansIncorrectThree;
-        }
     }
+    if(userChoice==0){
+        alert("The quiz will start now");
+        totalTime = 100;
+    }
+    A.style.visibility = 'visible';
+    B.style.visibility = 'visible';
+    C.style.visibility = 'visible';
+    D.style.visibility = 'visible';
+    console.log(userChoice);
+
+    
+    
+    var qq = allQuestions[userChoice];
+    document.getElementById("question").innerHTML = qq.question;
+    A.innerText = "a) " + qq.answers[0].ans;
+    A.value = qq.answers[0].result;
+    B.innerText = "b) " + qq.answers[1].ans;
+    B.value = qq.answers[1].result;
+    C.innerText = "c) " + qq.answers[2].ans;
+    C.value = qq.answers[2].result;
+    D.innerText = "d) " + qq.answers[3].ans;
+    D.value = qq.answers[3].result;
+
+    
+
+    A.onclick = buttonClick;
+    B.onclick = buttonClick; 
+    C.onclick = buttonClick;
+    D.onclick = buttonClick;  
+    submit.onclick = submission;
+    next.onclick = nextQ;
+    previous.onclick = previousQ;
+  
 }
